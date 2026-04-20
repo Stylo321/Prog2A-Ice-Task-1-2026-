58764bc4d86979cd213adf0b1a67ab3f7fd67638
Stylo <165194260+Stylo321@users.noreply.github.com>
2026-04-20 19:06:26 +0200

    Ice Task 1 ST10442843

---
+++ ProgrammingIce1/Cource.cs
@@ -0,0 +1,21 @@
+﻿public class Course
+{
+    private int _grade;
+
+    public int Grade
+    {
+        get { return _grade; }
+        set
+        {
+            if (value < 0 || value > 100)
+                throw new ArgumentException("Grade must be between 0 and 100.");
+
+            _grade = value;
+        }
+    }
+
+    public bool IsPass
+    {
+        get { return _grade >= 50; }
+    }
+}
---
+++ ProgrammingIce1/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Course course = new Course();
+        course.Grade = 75;
+
+        Console.WriteLine($"Grade: {course.Grade}");
+        Console.WriteLine($"Passed: {course.IsPass}");
+    }
+}
